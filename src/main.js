3d849c76dc8767c1a604f92370071672871166af
dtempo <dtempo16@gmail.com>
2019-06-24 11:59:58 +0600

    12 Making Layout

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import Vue from 'vue'
 import App from './App'
 window.$ = window.jQuery = require('jquery')
 import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 Vue.config.productionTip = false
 
